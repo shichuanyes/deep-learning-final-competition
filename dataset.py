c3c2c747a532f1c0ad2741bc4bddb1dae55496cc
shichuanyes <shichuanyes@gmail.com>
2023-11-24 00:11:11 -0500

    Fix indexing

--- dataset.py
+++ dataset.py
@@ -10,18 +10,18 @@ class ImageDataset(Dataset):
     def __init__(self, data_dir: str, num_frames: int = 22):
         self.data_dir = data_dir
         self.num_frames = num_frames
-        self.num_videos = sum(
-            os.path.isdir(os.path.join(self.data_dir, folder)) and folder.startswith('video_')
+        self.video_dirs = [
+            os.path.join(self.data_dir, folder)
             for folder in os.listdir(self.data_dir)
-        )
-        folder = os.listdir(self.data_dir)[0]
+            if os.path.isdir(os.path.join(self.data_dir, folder)) and folder.startswith('video_')
+        ]
 
     def __len__(self):
-        return self.num_videos * self.num_frames
+        return len(self.video_dirs) * self.num_frames
 
     def __getitem__(self, idx):
         img_idx = idx % self.num_frames
-        video_dir = os.path.join(self.data_dir, f'video_{idx // self.num_frames}')
+        video_dir = self.video_dirs[idx // self.num_frames]
         img_dir = os.path.join(video_dir, f'image_{img_idx}.png')
         masks = np.load(os.path.join(video_dir, 'mask.npy'))
         return {
